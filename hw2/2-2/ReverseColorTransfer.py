ffe028285b705d54d6f722feff5bc55e88c0d065
Edward <edward.yu0830@gmail.com>
2021-03-16 22:45:47 +0800

    開工，做逆向transfer

---
+++ hw2/2-2/ReverseColorTransfer.py
@@ -0,0 +1,18 @@
+import ColorTransfer as ct
+import sys
+
+
+def reverse_color_transfer(result, sideinform):
+    pass
+
+
+def main():
+    if len(sys.argv) < 3:
+        print(f'Usage: {sys.argv[0]} resultfile sideinformfile')
+    resultfile = sys.argv[1]
+    sideinformfile = sys.argv[2]
+
+
+
+if __name__ == '__main__':
+    main()
